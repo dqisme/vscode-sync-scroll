79490333d7b663c21751867945b9ec8503031708
DQ <dqisme@outlook.com>
2020-06-02 06:59:15 +0200

    [dq] format by removing semicolon.

--- src/extension.ts
+++ src/extension.ts
@@ -1,28 +1,28 @@
-import * as vscode from 'vscode';
+import * as vscode from 'vscode'
 
 const countLengthOfLineAt = (lineNumber: number, textEditor: vscode.TextEditor): number =>
-	textEditor.document.lineAt(lineNumber).range.end.character;
+	textEditor.document.lineAt(lineNumber).range.end.character
 
 const calculatePosition = (position: vscode.Position, scrollingEditor: vscode.TextEditor, scrolledEditor: vscode.TextEditor): vscode.Position =>
 	new vscode.Position(
 		position.line,
 		~~(position.character / countLengthOfLineAt(position.line, scrollingEditor) * countLengthOfLineAt(position.line, scrolledEditor)),
-	);
+	)
 
 const calculateRange = (visibleRange: vscode.Range, scrollingEditor: vscode.TextEditor, scrolledEditor: vscode.TextEditor): vscode.Range =>
 	new vscode.Range(
 		calculatePosition(visibleRange.start, scrollingEditor, scrolledEditor),
 		new vscode.Position(visibleRange.start.line + 1, 0),
-	);
+	)
 
 export function activate(context: vscode.ExtensionContext) {
-	let hasSplitPanels = vscode.window.visibleTextEditors.length > 1;
-	let scrollingTask: NodeJS.Timeout;
-	let scrollingEditor: vscode.TextEditor;
-	const scrolledEditorsQueue: Set<vscode.TextEditor> = new Set();
+	let hasSplitPanels = vscode.window.visibleTextEditors.length > 1
+	let scrollingTask: NodeJS.Timeout
+	let scrollingEditor: vscode.TextEditor
+	const scrolledEditorsQueue: Set<vscode.TextEditor> = new Set()
 	context.subscriptions.push(
 		vscode.window.onDidChangeVisibleTextEditors(textEditors => {
-			hasSplitPanels = textEditors.length > 1;
+			hasSplitPanels = textEditors.length > 1
 		}),
 		vscode.window.onDidChangeTextEditorVisibleRanges(({ textEditor, visibleRanges }) => {
 			if (!hasSplitPanels) {
@@ -30,24 +30,24 @@ export function activate(context: vscode.ExtensionContext) {
 			}
 			if (scrollingEditor !== textEditor) {
 				if (scrolledEditorsQueue.has(textEditor)) {
-					scrolledEditorsQueue.delete(textEditor);
-					return;	
+					scrolledEditorsQueue.delete(textEditor)
+					return	
 				}
-				scrollingEditor = textEditor;
+				scrollingEditor = textEditor
 			}
 			if (scrollingTask) {
-				clearTimeout(scrollingTask);
+				clearTimeout(scrollingTask)
 			}
 			scrollingTask = setTimeout(() => {
 				vscode.window.visibleTextEditors
 					.filter(editor => editor !== textEditor)
 					.forEach(editor => {
-						scrolledEditorsQueue.add(editor);
+						scrolledEditorsQueue.add(editor)
 						editor.revealRange(calculateRange(visibleRanges[0], textEditor, editor), vscode.TextEditorRevealType.AtTop)
 					})
-			}, 100);
+			}, 100)
 		}),
-	);
+	)
 }
 
 export function deactivate() {}
