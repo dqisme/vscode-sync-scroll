0c828624571056cb897dd7931f70f3fba5e13a9f
DQ <dqisme@outlook.com>
2020-06-02 08:06:45 +0200

    [dq] add toggle command and status bar item.

--- src/extension.ts
+++ src/extension.ts
@@ -1,4 +1,5 @@
 import * as vscode from 'vscode'
+const [toggleCommand] = require('../package.json').contributes.commands
 
 const countLengthOfLineAt = (lineNumber: number, textEditor: vscode.TextEditor): number =>
 	textEditor.document.lineAt(lineNumber).range.end.character
@@ -15,17 +16,52 @@ const calculateRange = (visibleRange: vscode.Range, scrollingEditor: vscode.Text
 		new vscode.Position(visibleRange.start.line + 1, 0),
 	)
 
+const checkSplitPanels = (textEditors: vscode.TextEditor[] = vscode.window.visibleTextEditors): boolean => textEditors.length > 1
+
 export function activate(context: vscode.ExtensionContext) {
-	let hasSplitPanels = vscode.window.visibleTextEditors.length > 1
 	let scrollingTask: NodeJS.Timeout
 	let scrollingEditor: vscode.TextEditor
 	const scrolledEditorsQueue: Set<vscode.TextEditor> = new Set()
+
+	// Status bar item
+	let hasSplitPanels: boolean = checkSplitPanels()
+	const statusBatSwitch: vscode.StatusBarItem = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Right, 200)
+	statusBatSwitch.tooltip = toggleCommand.title
+	statusBatSwitch.command = toggleCommand.command
+	const refreshStatusBarItem = () => {
+		if (hasSplitPanels) {
+			statusBatSwitch.show()
+		} else {
+			statusBatSwitch.hide()
+		}
+	}
+
+	// Switch to turn on/off
+	let isOn: boolean;
+	const toggleOn = () => {
+		isOn = true;
+		statusBatSwitch.text = 'Sync Scroll: ON'
+	}
+	const toggleOff = () => {
+		isOn = false;
+		statusBatSwitch.text = 'Sync Scroll: OFF'
+	}
+
+	// Register disposables
 	context.subscriptions.push(
+		vscode.commands.registerTextEditorCommand(toggleCommand.command, () => {
+			if (isOn) {
+				toggleOff()
+			} else {
+				toggleOn()
+			}
+		}),
 		vscode.window.onDidChangeVisibleTextEditors(textEditors => {
-			hasSplitPanels = textEditors.length > 1
+			hasSplitPanels = checkSplitPanels(textEditors)
+			refreshStatusBarItem()
 		}),
 		vscode.window.onDidChangeTextEditorVisibleRanges(({ textEditor, visibleRanges }) => {
-			if (!hasSplitPanels) {
+			if (!hasSplitPanels || !isOn) {
 				return
 			}
 			if (scrollingEditor !== textEditor) {
@@ -48,6 +84,10 @@ export function activate(context: vscode.ExtensionContext) {
 			}, 100)
 		}),
 	)
+
+	// Init
+	toggleOn()
+	refreshStatusBarItem()
 }
 
 export function deactivate() {}
